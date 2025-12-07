3b5285784ff1b66436fbb2ed752c33d21f093202
jrwishart93 <jrwishart@hotmail.co.uk>
2025-12-07 15:18:41 +0000

    Add voting icons assets and preview

---
+++ src/assets/votingIcons.js
@@ -0,0 +1,9 @@
+// src/assets/votingIcons.js
+
+export const VOTING_ICONS = {
+  firstHome: "public/voting-icons/voting-first-home.svg",
+  lastStanding: "public/voting-icons/voting-last-standing.svg",
+  ninjaExit: "public/voting-icons/voting-ninja-exit.svg",
+  mostDrunk: "public/voting-icons/voting-most-drunk.svg",
+  mostAlcohol: "public/voting-icons/voting-most-alcohol.svg",
+};
